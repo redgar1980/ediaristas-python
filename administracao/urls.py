0543c3fd021bac105b5af2aefdcc19b6199be24b
Rodrigo Custodio <rodrigocustodio2023@hotmail.com>
2024-06-27 19:26:40 -0300

    ativando e personalizando a página de login

--- administracao/urls.py
+++ administracao/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import servico_views, usuario_views
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('servicos/cadastrar', servico_views.cadastrar_servico, name='cadastrar_servico'),
     path('servicos/listar', servico_views.listar_servicos, name='listar_servicos'),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('usuarios/cadastrar', usuario_views.cadastrar_usuario, name='cadastrar_usuario'),
     path('usuarios/listar', usuario_views.listar_usuarios, name='listar_usuarios'),
     path('usuarios/editar/<int:id>', usuario_views.editar_usuario, name='editar_usuario'),
+    path('autenticacao/login', auth_views.LoginView.as_view(), name='logar_usuario'),
 ]
--- ediaristas/settings.py
+++ ediaristas/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+LOGIN_REDIRECT_URL = 'listar_servicos'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
