88d5a3e4fd4fef7bc9ce1e124032b8f8468587cc
Rodrigo Custodio <rodrigocustodio2023@hotmail.com>
2024-08-06 19:14:03 -0300

    Criando o model de diária

--- api/models.py
+++ api/models.py
@@ -7,6 +7,7 @@ from django.core.validators import validate_image_file_extension
 from localflavor.br.models import BRCPFField 
 from .managers import diarista_manager
 from django.contrib.auth.models import UserManager
+from administracao.models import Servico
 
 def nome_arquivo_foto(instance, filename):
     ext = filename.split('.')[-1]
@@ -46,6 +47,49 @@ class Usuario(AbstractUser):
 
     objects = UserManager()
     diaristas_objects = diarista_manager.DiaristaManager()
+
+class Diaria(models.Model):
+    STATUS_DIARIA_CHOICES = (
+        (1, 'SEM_PAGAMENTO'),
+        (2, 'PAGO'),
+        (3, 'CONFIRMADO'),
+        (4, 'CONCLUIDO'),
+        (5, 'CANCELADO'),
+        (6, 'AVALIADO'),
+        (7, 'TRANSFERIDO')
+    )
+
+    data_atendimento = models.DateField(null=False, blank=False)
+    tempo_atendimento = models.IntegerField(null=False, blank=False)
+    status = models.IntegerField(null=False, choices=STATUS_DIARIA_CHOICES,
+        default=1)
+    preco = models.FloatField(null=False, blank=False)
+    valor_comissao = models.FloatField(null=False, blank=False)
+    logradouro = models.CharField(max_length=60, null=False, blank=False)
+    numero = models.CharField(max_length=10, null=False, blank=False)
+    bairro = models.CharField(max_length=30, null=False, blank=False)
+    complemento = models.CharField(max_length=100, null=False, blank=False)
+    cep = models.CharField(max_length=10, null=False, blank=False)
+    cidade = models.CharField(max_length=30, null=False, blank=False)
+    estado = models.CharField(max_length=2, null=False, blank=False)
+    codigo_ibge = models.IntegerField(null=True, blank=False)
+    quantidade_quartos = models.IntegerField(null=False, blank=False)
+    quantidade_salas = models.IntegerField(null=False, blank=False)
+    quantidade_cozinhas = models.IntegerField(null=False, blank=False)
+    quantidade_banheiros = models.IntegerField(null=False, blank=False)
+    quantidade_quintais = models.IntegerField(null=False, blank=False)
+    quantidade_outros = models.IntegerField(null=False, blank=False)
+    observacoes = models.TextField(null=False, blank=True)
+    motivo_cancelamento = models.TextField(null=True, blank=True)
+    cliente = models.ForeignKey(Usuario, null=False, blank=False, 
+        on_delete=models.DO_NOTHING, related_name='cliente')
+    diarista = models.ForeignKey(Usuario, null=True, blank=True, 
+        on_delete=models.DO_NOTHING, related_name='diarista')
+    servico = models.ForeignKey(Servico, null=False, blank=False, 
+        on_delete=models.DO_NOTHING)
+    candidatos = models.ManyToManyField(Usuario, blank=True, related_name='candidatos')
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    update_at = models.DateTimeField(auto_now=True, null=True)
 
 class CidadesAtendimento(models.Model):
     codigo_ibge = models.IntegerField(null=False, blank=False)
--- api/signals.py
+++ api/signals.py
@@ -4,15 +4,15 @@ from django.template.loader import render_to_string
 from .models import Usuario
 
 def usuario_cadastrado(sender, instance, created, **kwargs):
-    #print("Usuário cadastrado")
-    #print(instance.nome_completo)
-    if created:
-        assunto = 'Cadastro realizado com sucesso'
-        corpo_email = ''
-        email_destino = [instance.email]
-        email_remetente = 'rodrigocustodio2023@hotmail.com'
-        mensagem_html = render_to_string('email_cadastro.html', {'usuario': instance})
-        send_mail(assunto, corpo_email, email_remetente, email_destino, html_message=mensagem_html)
+    print("Usuário cadastrado")
+    print(instance.nome_completo)
+    # if created:
+    #     assunto = 'Cadastro realizado com sucesso'
+    #     corpo_email = ''
+    #     email_destino = [instance.email]
+    #     email_remetente = 'rodrigocustodio2023@hotmail.com'
+    #     mensagem_html = render_to_string('email_cadastro.html', {'usuario': instance})
+    #     send_mail(assunto, corpo_email, email_remetente, email_destino, html_message=mensagem_html)
 
 
 post_save.connect(usuario_cadastrado, sender=Usuario)
