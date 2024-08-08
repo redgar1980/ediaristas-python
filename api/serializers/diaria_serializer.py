417cc8d8f313f2e42cff7fbce03bd226bda678ec
Rodrigo Custodio <rodrigocustodio2023@hotmail.com>
2024-08-08 20:31:50 -0300

    Validando a disponibilidade de cidade

--- api/serializers/diaria_serializer.py
+++ api/serializers/diaria_serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from ..models import Diaria, Usuario
 from administracao.services import servico_service
+from ..services.cidades_atendimento_service import verificar_disponibilidade_cidade
 
 class UsuarioDiariaSerializer(serializers.ModelSerializer):
     class Meta:
@@ -21,6 +22,12 @@ class DiariaSerializer(serializers.ModelSerializer):
         diaria = Diaria.objects.create(valor_comissao=valor_comissao,
             cliente_id=self.context['request'].user.id,**validated_data)
         return diaria
+    
+    def validate(self, attrs):
+        if not verificar_disponibilidade_cidade(attrs['cep']):
+            raise serializers.ValidationError("Não há diaristas para o CEP informado")
+        return attrs
+
     
     def validate_preco(self, preco):
         servico = servico_service.listar_servico_id(self.initial_data["servico"])
