9cd899cd18c270af4b41202d9268af4a7ccbd887
Rodrigo Custodio <rodrigocustodio2023@hotmail.com>
2024-08-11 16:04:13 -0300

    Busca diária do usuário

--- api/services/diaria_service.py
+++ api/services/diaria_service.py
@@ -1,10 +1,16 @@
 from ..models import Diaria
+from .usuario_service import listar_usuario_id
 
 def listar_diaria_id(diaria_id):
     return Diaria.objects.get(id=diaria_id)
-
 
 def atualizar_status_diaria(diaria_id, status):
     diaria = listar_diaria_id(diaria_id)
     diaria.status = status
     diaria.save()
+
+def listar_diarias_usuario(usuario_id):
+    usuario = listar_usuario_id(usuario_id)
+    if usuario.tipo_usuario == 1:
+        return Diaria.objects.filter(cliente=usuario.id).all()
+    return Diaria.objects.filter(diarista=usuario_id).all()
