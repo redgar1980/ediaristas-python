b0e46c6b10e34b5f9fa207da16caac7765e21388
Rodrigo Custodio <rodrigocustodio2023@hotmail.com>
2024-08-11 19:40:14 -0300

    Listando diária por ID

--- api/urls.py
+++ api/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
      path('usuarios', usuario_view.Usuario.as_view(), name='usuario-list'),
      path('me', me_view.Me.as_view(), name='me-list'),
      path('diarias', diaria_view.Diaria.as_view(), name='diaria-list'),
+     path('diarias/<int:diaria_id>', diaria_view.DiariaID.as_view(), name='diaria-detail'),
      path('diarias/<int:diaria_id>/pagamentos', pagamento_diaria_view.PagamentoDiaria.as_view(), 
           name='pagamento-diaria-list')
 ]
