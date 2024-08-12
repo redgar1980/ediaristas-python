08ee78114b88d1071f4ee0d449ddf9f409f577e7
Rodrigo Custodio <rodrigocustodio2023@hotmail.com>
2024-08-12 20:01:46 -0300

    Criando Serializer de cidades de atendimento de diarista

---
+++ api/serializers/cidades_atendimento_diarista_serializer.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from ..models import CidadesAtendimento
+
+class CidadesAtendimentoDiaristaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CidadesAtendimento
+        fields = '__all__'
