9a6415ed39dd06d4bc625341a14720ec0a47e4a9
Rodrigo Custodio <rodrigocustodio2023@hotmail.com>
2024-08-22 20:56:27 -0300

    Confirmando presença de diarista

--- api/services/diaria_service.py
+++ api/services/diaria_service.py
@@ -1,6 +1,8 @@
 from ..models import Diaria
 from .usuario_service import listar_usuario_id
 from .endereco_diarista_service import listar_endereco_diarista
+from rest_framework import serializers
+import datetime
 
 def listar_diaria_id(diaria_id):
     return Diaria.objects.get(id=diaria_id)
@@ -22,4 +24,15 @@ def calcular_indice_compatibilidade(diaria_id, diarista_id):
     reputacao_diarista = diarista.reputacao
     #endereco_diarista = listar_endereco_diarista(diarista_id)
     distancia_diarista_diaria = 100 # depois implementar o método para pegar a distância
-    return  (reputacao_diarista - (distancia_diarista_diaria/10)) / 2    
+    return  (reputacao_diarista - (distancia_diarista_diaria/10)) / 2
+
+def confirmar_presenca_diarista(diaria_id):
+    diaria = listar_diaria_id(diaria_id)
+    if diaria.status != 3:
+        raise serializers.ValidationError("Só é possível executar essa ação em diárias com status 3")
+    data_atual = datetime.datetime.now()
+    data_diaria = diaria.data_atendimento.replace(tzinfo=None)
+    if data_atual <= data_diaria:
+        raise serializers.ValidationError("A data atual não pode ser menor que a data de atendimento")
+    diaria.status = 4
+    diaria.save()
--- api/views/confirmar_presenca_diarista_view.py
+++ api/views/confirmar_presenca_diarista_view.py
@@ -5,10 +5,11 @@ from ..permissions import cliente_permission
 from ..services import diaria_service
 
 class ConfirmarPresencaDiaristaID(APIView):
-    permission_classes = [cliente_permission]
+    permission_classes = [cliente_permission.ClientePermission]
 
     def patch(self, request, diaria_id, format=None):
         diaria = diaria_service.listar_diaria_id(diaria_id)
         self.check_object_permissions(self.request, diaria)
         # implementar no service a lógica para confirmar a presenca
+        diaria_service.confirmar_presenca_diarista(diaria_id)
         return Response("Presença confirmada com sucesso", status=status_http.HTTP_200_OK)
