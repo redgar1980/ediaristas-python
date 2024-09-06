a494a147462a86162f398cf7aaf3873904bf4ad3
Rodrigo Custodio <rodrigocustodio2023@hotmail.com>
2024-09-06 20:56:07 -0300

    Criando Task cancelar diária e enviar email de oportunidades e candidatar diária

--- api/signals.py
+++ api/signals.py
@@ -10,39 +10,43 @@ def usuario_cadastrado(sender, instance, created, **kwargs):
     #     assunto = 'Cadastro realizado com sucesso'
     #     corpo_email = ''
     #     email_destino = [instance.email]
-    #     email_remetente = 'rodrigocustodio2023@hotmail.com'
+    #     email_remetente = 'talentoinfodesenvolvimento@gmail.com'
     #     mensagem_html = render_to_string('email_cadastro.html', {'usuario': instance})
     #     send_mail(assunto, corpo_email, email_remetente, email_destino, html_message=mensagem_html)
 
 def diarista_selecionada(sender, instance, **kwargs):
     if instance.status == 3:
-       html_message_cliente = render_to_string('email_diarista_selecionada.html',
-            {'usuario': instance.cliente, 'diaria': instance})
-       html_message_diarista = render_to_string('email_diarista_selecionada.html',
-            {'usuario': instance.cliente, 'diaria': instance})
-       email_remetente = 'rodrigocustodio2023@hotmail.com'
-       email_destino_cliente = [instance.cliente.email, ]
-       email_destino_diarista = [instance.diarista.email, ]
-       assunto = 'Diarista selecionada'
-       corpo_email = ''
-       send_mail(assunto, corpo_email, email_remetente, email_destino_cliente, 
-            html_message=html_message_cliente)
-       send_mail(assunto, corpo_email, email_remetente, email_destino_diarista, 
-            html_message=html_message_diarista)
+        print("Diária com status 3")
+        print(instance.cep)
+    #    html_message_cliente = render_to_string('email_diarista_selecionada.html',
+    #         {'usuario': instance.cliente, 'diaria': instance})
+    #    html_message_diarista = render_to_string('email_diarista_selecionada.html',
+    #         {'usuario': instance.cliente, 'diaria': instance})
+    #    email_remetente = 'talentoinfodesenvolvimento@gmail.com'
+    #    email_destino_cliente = [instance.cliente.email, ]
+    #    email_destino_diarista = [instance.diarista.email, ]
+    #    assunto = 'Diarista selecionada'
+    #    corpo_email = ''
+    #    send_mail(assunto, corpo_email, email_remetente, email_destino_cliente, 
+    #         html_message=html_message_cliente)
+    #    send_mail(assunto, corpo_email, email_remetente, email_destino_diarista, 
+    #         html_message=html_message_diarista)
 
 def nova_oportunidade(sender, instance, **kwargs):
     if instance.status == 2:
-        cidade_atendimento = CidadesAtendimento.objects.get(codigo_ibge=instance.codigo_ibge)
-        diaristas = cidade_atendimento.usuario.all().values('email')
-        html_message_diarista = render_to_string('email_nova_oportunidade.html')
-        email_remetente = 'rodrigocustodio2023@hotmail.com'
-        assunto = 'Nova oportunidade no ediaristas'
-        corpo_email = ''
-        lista_email = []
-        for email in diaristas:
-            lista_email.append(email['email'])
-        send_mail(assunto, corpo_email, email_remetente, lista_email,
-            html_message=html_message_diarista)
+        print("Diária com status 3")
+        print(instance.cep)
+        # cidade_atendimento = CidadesAtendimento.objects.get(codigo_ibge=instance.codigo_ibge)
+        # diaristas = cidade_atendimento.usuario.all().values('email')
+        # html_message_diarista = render_to_string('email_nova_oportunidade.html')
+        # email_remetente = 'talentoinfodesenvolvimento@gmail.com'
+        # assunto = 'Nova oportunidade no ediaristas'
+        # corpo_email = ''
+        # lista_email = []
+        # for email in diaristas:
+        #     lista_email.append(email['email'])
+        # send_mail(assunto, corpo_email, email_remetente, lista_email,
+        #     html_message=html_message_diarista)
 
 post_save.connect(usuario_cadastrado, sender=Usuario)
 post_save.connect(diarista_selecionada, sender=Diaria)
