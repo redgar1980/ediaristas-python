cc691a7fadea727f213b13cb3eaeb872362cfc74
Rodrigo Custodio <rodrigocustodio2023@hotmail.com>
2024-09-12 20:16:23 -0300

    Transferido pagamento

--- administracao/views/diaria_views.py
+++ administracao/views/diaria_views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.urls.base import reverse
 from api.models import Diaria
 
 @login_required
 def lista_diarias(request):
     diarias = Diaria.objects.all()
     return render(request, 'diarias/lista_diarias.html', {"diarias": diarias})
+
+def transferir_pagamento_diaria(request, diaria_id):
+    diaria = Diaria.objects.get(id=diaria_id)
+    diaria.status = 7
+    diaria.save()
+    return HttpResponseRedirect(reverse('lista_diarias'))
