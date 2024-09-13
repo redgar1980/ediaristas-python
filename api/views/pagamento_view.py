98a8b223d9961f8cf899e22cc2c4e3fae4f5eac4
Rodrigo Custodio <rodrigocustodio2023@hotmail.com>
2024-09-13 20:28:27 -0300

    Correção listando pagamentos api - parte 2

--- api/views/pagamento_view.py
+++ api/views/pagamento_view.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status as status_http
-from ..models import Diaria, Pagamento
+from ..models import Diaria, Pagamento as PagamentoModel
 from ..serializers import pagamento_serializer
 from ..permissions import diarista_permission
 
@@ -10,7 +10,7 @@ class Pagamento(APIView):
     def get(self, request, format=None):
         # pegar a lista de pagamentos do usuário logado
         diarias_pagamento = Diaria.objects.filter(diarista=request.user.id).filter(status__in=[4, 6, 7])
-        pagamentos = Pagamento.objects.filter(diaria__in=diarias_pagamento)
+        pagamentos = PagamentoModel.objects.filter(diaria__in=diarias_pagamento)
         # serializar os dados
         serializer_pagamento = pagamento_serializer.PagamentoSerializer(pagamentos, many=True)
         # retornar os pagamentos do usuário
