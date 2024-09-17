c159493ff35319d7e5796517b915827c1b49bf11
Rodrigo Custodio <rodrigocustodio2023@hotmail.com>
2024-09-17 19:00:42 -0300

    Adicionando permissão de edição de usuário

--- api/views/usuario_view.py
+++ api/views/usuario_view.py
@@ -1,10 +1,11 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from ..serializers import usuario_serializer, editar_usuario_serializer
-from rest_framework.permissions import IsAuthenticated
+from ..permissions import editar_usuario_permission
 from rest_framework import status as status_http
 
 class Usuario(APIView):
+    permission_classes = [editar_usuario_permission.EditarUsuarioPermission, ]
     def post(self, request, format=None):
         serializer_usuario = usuario_serializer.UsuarioSerializer(data=request.data,
                                                         context={"request": request})
@@ -17,7 +18,7 @@ class Usuario(APIView):
     def put(self, request, format=None):
         usuario_antigo = Usuario.objects.get(id=request.user.id)
         serializer_usuario = editar_usuario_serializer.EditarUsuarioSerializer(
-            usuario_serializer, data=request.data, context={"request": request})
+            usuario_antigo, data=request.data, context={"request": request})
         if serializer_usuario.is_valid():
             serializer_usuario.save()
             return Response(serializer_usuario.data, status=status_http.HTTP_200_OK)
